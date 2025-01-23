e391babfdbdd8ae78ea0652a00b166878b20a121
PHCM <phchavesmaia@gmail.com>
2025-01-22 23:08:26 -0300

    trying to get x-axis entries

--- models/helpman-1998/primer.jl
+++ models/helpman-1998/primer.jl
@@ -16,42 +16,55 @@ end
 cd("D:/Dropbox/learn-julia/qse/models/helpman-1998/")
 load_dir("functions")
 
-# ****************************
-# **** Transaction Costs  **** 
-# ****************************
+# ********************
+# **** Distances  **** 
+# ********************
 
 # determining the graph representative of the 30x30 grid
 N=30
-diagonal_cost = 2^0.5;
-straight_cost = 1.0;
+diagonal_cost = 2^0.5
+straight_cost = 1.0
 A, A_weights = adjacency_matrix(N, diagonal_cost, straight_cost)
 G = Graph(A)
 gplot(G)
 
-# estimating the least cost path matrix
+# estimating the "least cost path (distance)" matrix 
+distₙᵢ = zeros(N*N,N*N)
 for j ∈ 1:N*N
-    dist = dijkstra_shortest_paths(G,j,A_weights).dists
-    if j==1
-        πᵢⱼ = dist
-    else
-        πᵢⱼ = hcat(πᵢⱼ,dist)
-    end
+    distₙᵢ[:,j] = dijkstra_shortest_paths(G,j,A_weights).dists
 end
 
 # ***********************
 # **** Productivity  **** 
 # ***********************
-using CSV ,DataFrames, GeoStats
-import CairoMakie as Mke
+using CSV ,DataFrames, GeoStats, CairoMakie
 
 Aᵢ = CSV.read("a.csv", DataFrame; header = false) 
 rename!(Aᵢ,[:Productivity])
-
-# plotting figure 1
 grid = CartesianGrid(30, 30)
 Aᵢ = georef(Aᵢ, grid)
 
-fig = Mke.Figure()
-viz(fig[1,1], Aᵢ.geometry, color = Aᵢ.Productivity, colormap = "viridis")
-cbar(fig[1,2], Aᵢ.Productivity, colormap = "viridis")
+# plotting figure 1
+fig = Figure()
+ax = Axis(fig[1, 1], 
+              title = "Log productivity",
+              titlealign = :left, 
+              xlabel = "Longitude", 
+              ylabel = "Latitude")
+viz!(ax, Aᵢ.geometry, color = Aᵢ.Productivity, colormap = :viridis)
+Colorbar(fig[1, 2], limits = (minimum(Aᵢ.Productivity), maximum(Aᵢ.Productivity)), 
+        colormap = :viridis, flipaxis = true, ticks=-3:1:3, label="Log points", flip_vertical_label=true)
 fig
+
+# *********************
+# **** Parameters  **** 
+# *********************
+
+α = 0.25 # Davis & Ortalo-Magne (2011)
+σ = 5 # Simonovska & Waugh (2014)
+ϕ = 0.375 # Based on international trade data
+dₙᵢ = distₙᵢ .^ ϕ # trade costs are a constant elasticity function of effective distance by assumption 
+τⁱⁿ = 2 # by assumption
+
+Aᵢ = @transform(Aᵢ, :center = centroid(:geometry))
+@transform(Aᵢ, :X = first(coordinates(:center)))
